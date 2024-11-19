420d0342df4af2f50d335ac58a741e4c1e7055e2
heather <heather.soper63@gmail.com>
2024-11-19 19:23:50 +0800

    Update Customer Purchase History.sql

--- Reports Generated/Customer Purchase History.sql
+++ Reports Generated/Customer Purchase History.sql
@@ -5,12 +5,12 @@ SELECT c.customer ID,
        c.first_name,
        c.email,
        c.phone_number,
-       YEAR(o.order_date) AS order_year,
-       MONTH(o.order_date) AS order_month,
+       YEAR(o.order_date) AS year,
+       MONTH(o.order_date) AS month,
        COUNT(o.orderID) AS total_purchases,
        SUM(o.order_total) AS amount_spent
-FROM   customers c 
+FROM   customer c 
 LEFT JOIN orders o ON c.customerID = o.customerID
 WHERE o.order_status = 'Completed'
-GROUP BY c.customerID, YEAR(o.order_date), MONTH(o.order_date)
-ORDER BY c.customerID, order_year, order_month;
+GROUP BY c.customerID, c.first_name, c.last_name
+ORDER BY amount_spent DESC;
