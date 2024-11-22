84a6e6b411c26a114b5f7e475b689fb188acf312
heather <heather.soper63@gmail.com>
2024-11-22 23:14:30 +0800

    Add files via upload

--- Reports Generated/Customer Purchase History.sql
+++ Reports Generated/Customer Purchase History.sql
@@ -1,6 +1,6 @@
 USE ECommerceProject;
 
-SELECT c.customer ID,
+SELECT c.customerID,
        c.last_name,
        c.first_name,
        c.email,
@@ -14,3 +14,18 @@ LEFT JOIN orders o ON c.customerID = o.customerID
 WHERE o.order_status = 'Completed'
 GROUP BY c.customerID, c.first_name, c.last_name
 ORDER BY amount_spent DESC;
+
+SELECT c.customerID,
+	   c.last_name, 
+       c.first_name, 
+       c.email,
+       c.phone_number,
+       YEAR(o.order_date) AS order_year,
+       MONTH(o.order_date) AS order_month,
+       COUNT(o.orderID) AS total_purchases,
+       SUM(o.order_total) AS amount_spent 
+FROM customer c 
+LEFT JOIN orders o ON c.customerID = o.customerID
+WHERE o.order_status = 'Completed'
+GROUP BY c.customerID, YEAR(o.order_date), MONTH(o.order_date)
+ORDER BY c.customerID, order_year, order_month;
